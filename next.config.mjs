4f25461167a645fad6cf107de3880c1cdfc27371
Ye Yangchen <eikasia30@gmail.com>
2024-03-24 23:12:08 -0500

    try a little trick to fix css not working in prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.optimization.splitChunks.cacheGroups = {
+      common: {
+        name: "common",
+        chunks: "all",
+      },
+    };
+
+    return config;
+  },
+};
 
 export default nextConfig;
