59fea6848be6df09392e76173a7f2b160ba812c0
Eikasia30 <eikasia30@gmail.com>
2024-05-11 16:11:45 +0800

    feat: 备案图标 (#28)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Heading from "./components/Heading";
+import Image from "next/image";
 import MainNav from "./components/MainNav";
 import SongPanel from "./components/SongPanel";
 import StickyHeader from "@/components/StickyHeader";
@@ -29,8 +30,38 @@ export default async function Home() {
         </StickyHeader>
         <Heading songCount={songCount} />
         <SongPanel allSongs={songs} />
-        <div className="mt-2 p-4 text-center text-sm text-thin text-black">
+        <div className="mt-2 mb-2 p-4 text-center text-sm text-thin text-black">
           Copyright © 2023-2024 梦中杀蝶人协会 & 他们的朋友
+        </div>
+        <div className="w-full text-center text-sm text-light text-black">
+          <span className="inline-block overflow-hidden border-0 mx-0 p-0">
+            <Image
+              src="/icons/备案.png"
+              alt="备案图标"
+              width={30}
+              height={30}
+              className="rounded-full"
+              priority={true}
+            ></Image>
+          </span>
+          <footer>
+            <a
+              href="https://beian.mps.gov.cn/#/query/webSearch?code=61019702000415"
+              rel="noreferrer"
+              target="_blank"
+            >
+              陕公网安备61019702000415号
+            </a>
+          </footer>
+          <footer>
+            <a
+              href="https://beian.miit.gov.cn/#/Integrated/index"
+              rel="noreferrer"
+              target="_blank"
+            >
+              陕ICP备2023014792号-1
+            </a>
+          </footer>
         </div>
       </div>
     </div>
