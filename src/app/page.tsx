4ecaa0a7de2944ec884ac8d58b86f1178bcc00ac
Yangchen Ye <eikasia30@gmail.com>
2024-07-07 10:08:13 -0400

    fix: background resizing & on iPad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Root from "./Root";
 
 import { readSongAllNoCacheLatest } from "./actions/crud";
@@ -14,5 +15,22 @@ export default async function Home() {
 			artist: song.artist,
 		}));
 
-	return <Root songs={songs} tracks={tracks} profile={vtuberProfile} />;
+	return (
+		<div className="p-0">
+			<Root songs={songs} tracks={tracks} profile={vtuberProfile} />
+			{vtuberProfile.backgroundImagePath && (
+				<div className="fixed top-0 left-0 h-full w-full overflow-hidden pointer-events-none -z-10">
+					<Image
+						src={vtuberProfile.backgroundImagePath}
+						alt="background"
+						width={0}
+						height={0}
+						sizes="100vw"
+						style={{ width: "100%", height: "100%" }}
+						className="absolute inset-0 opacity-45 object-cover"
+					/>
+				</div>
+			)}
+		</div>
+	);
 }
