33fdd8be6add5f04f0f8a0a834bf41b4fc903a83
Eikasia30 <eikasia30@gmail.com>
2024-07-07 10:10:56 -0400

    fix: background resizing & on iPad (#44)

--- src/app/Root.tsx
+++ src/app/Root.tsx
@@ -11,7 +11,6 @@ import type { Track } from "@/lib/player";
 import UserNav from "./components/UserNav";
 import type { VtuberProfile } from "@/profile";
 
-import { cn } from "@/lib/utils";
 import dynamic from "next/dynamic";
 import { useState } from "react";
 
@@ -35,72 +34,65 @@ export default function Root({ songs, tracks, profile }: PropType) {
 		setPlayerVisible(false);
 	};
 
-	// TODO: why template string doesn't work here?
-	const backgroundClassName = profile.backgroundImagePath
-		? `bg-gradient-to-r from-[#c6b1f0]/50 to-white/50 md:bg-none md:before:absolute md:before:-z-10 md:before:bg-[url('/img/background-compressed.webp')] md:before:bg-cover md:before:bg-fixed md:before:opacity-45 md:before:inset-0`
-		: "bg-gradeint-to-r from-[#c6b1f0]";
-
 	return (
 		<div className="relative font-chinese bg-opacity-50">
-			<div className={cn(backgroundClassName)}>
-				<StickyHeader>
-					<div className="container flex h-14 items-center">
-						<MainNav className="mx-6" profile={profile} />
-						<div className="ml-auto flex items-center">
-							<span className="mr-2">
-								<div
-									className="font-medium text-muted-foreground transition-colors hover:text-primary cursor-pointer"
-									onClick={() => setPlayerVisible((val) => !val)}
-								>
-									<Icons.music_note className="inline align-top" />
-									<span className="hidden md:inline">播放器</span>
-								</div>
-							</span>
-							<UserNav profile={profile} />
-						</div>
+			<StickyHeader>
+				<div className="container flex h-14 items-center">
+					<MainNav className="mx-6" profile={profile} />
+					<div className="ml-auto flex items-center">
+						<span className="mr-2">
+							<div
+								className="font-medium text-muted-foreground transition-colors hover:text-primary cursor-pointer"
+								onClick={() => setPlayerVisible((val) => !val)}
+							>
+								<Icons.music_note className="inline align-top" />
+								<span className="hidden md:inline">播放器</span>
+							</div>
+						</span>
+						<UserNav profile={profile} />
 					</div>
-				</StickyHeader>
-				<Heading songCount={songCount} profile={profile} />
-				<SongPanel allSongs={songs} />
-				<div className="mt-2 mb-2 p-4 text-center text-sm text-thin text-black">
-					Copyright © 2023-2024 梦中杀蝶人协会 & 他们的朋友
 				</div>
-				<div className="w-full text-center text-sm text-light text-black">
-					<span className="inline-block overflow-hidden border-0 mx-0 p-0">
-						<Image
-							src="/icons/备案.png"
-							alt="备案图标"
-							width={30}
-							height={30}
-							className="rounded-full"
-							priority={true}
-						></Image>
-					</span>
-					<footer>
-						<a
-							href="https://beian.mps.gov.cn/#/query/webSearch?code=61019702000415"
-							rel="noreferrer"
-							target="_blank"
-						>
-							陕公网安备61019702000415号
-						</a>
-					</footer>
-					<footer>
-						<a
-							href="https://beian.miit.gov.cn/#/Integrated/index"
-							rel="noreferrer"
-							target="_blank"
-						>
-							陕ICP备2023014792号-1
-						</a>
-					</footer>
-				</div>
-				<SongPlayer
-					visible={playerVisible}
-					closePlayer={closePlayer}
-					tracks={tracks}
-				/>
+			</StickyHeader>
+			<Heading songCount={songCount} profile={profile} />
+			<SongPanel allSongs={songs} />
+			<div className="mt-2 mb-2 p-4 text-center text-sm text-thin text-black">
+				Copyright © 2023-2024 梦中杀蝶人协会 & 他们的朋友
 			</div>
+			<div className="w-full text-center text-sm text-light text-black">
+				<span className="inline-block overflow-hidden border-0 mx-0 p-0">
+					<Image
+						src="/icons/备案.png"
+						alt="备案图标"
+						width={30}
+						height={30}
+						className="rounded-full"
+						priority={true}
+					></Image>
+				</span>
+				<footer>
+					<a
+						href="https://beian.mps.gov.cn/#/query/webSearch?code=61019702000415"
+						rel="noreferrer"
+						target="_blank"
+					>
+						陕公网安备61019702000415号
+					</a>
+				</footer>
+				<footer>
+					<a
+						href="https://beian.miit.gov.cn/#/Integrated/index"
+						rel="noreferrer"
+						target="_blank"
+					>
+						陕ICP备2023014792号-1
+					</a>
+				</footer>
+			</div>
+			<SongPlayer
+				visible={playerVisible}
+				closePlayer={closePlayer}
+				tracks={tracks}
+			/>
 		</div>
 	);
 }
