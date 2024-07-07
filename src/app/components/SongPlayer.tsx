2b3baaa311ea28c6ac831e45ba069d46c9a24757
Eikasia30 <eikasia30@gmail.com>
2024-07-06 22:47:45 -0400

    feat: animate player (#42)

--- src/app/components/SongPlayer.tsx
+++ src/app/components/SongPlayer.tsx
@@ -11,6 +11,7 @@ import getPlayerSingleton, {
 import { Slider } from "@/components/ui/slider";
 
 import { formatMMSS } from "@/lib/utils";
+import { motion } from "framer-motion";
 import { useEffect } from "react";
 
 interface PropType {
@@ -63,61 +64,62 @@ export default function SongPlayer({ visible, tracks, closePlayer }: PropType) {
 	};
 
 	return (
-		<div className="fixed bottom-0 left-0 w-full">
-			{visible && (
-				<div className="bottom-0 left-0 w-full shadow-lg p-0 border border-hikari_blue/20 bg-background/95">
-					<div className="flex items-center justify-between max-w-[500px] md:max-w-[500px] m-auto">
-						<div className="flex justify-between items-center p-1">
-							<Icons.player_prev_button
-								onClick={() => player.prev()}
-								className="fill-black hover:fill-gray-600"
-							/>
-							<PlayOrPauseIcon
-								onClick={handlePlay}
-								className="ml-2 fill-black hover:fill-gray-600"
-							/>
-							<Icons.player_next_button
-								onClick={() => player.next()}
-								className="ml-2 fill-black hover:fill-gray-600"
-							/>
-						</div>
-						<div className="flex flex-col w-full h-full p-0 justify-start mx-2">
-							<div className="flex h-1/2 justify-between">
-								<div className="flex justify-center text-sm md:max-w-[500px] h-full mb-2">
-									<div className="leading-4">
-										{currentTrack ? currentTrack.title : "点击播放键开始播放"}
-									</div>
-									<div className="leading-4 ml-2">
-										{currentTrack ? currentTrack.artist : ""}
-									</div>
+		<motion.div
+			className="fixed bottom-0 left-0 w-full"
+			animate={{ y: visible ? 0 : 100 }}
+		>
+			<div className="w-full shadow-lg p-0 border border-hikari_blue/20 bg-background/95">
+				<div className="flex items-center justify-between max-w-[500px] md:max-w-[500px] m-auto">
+					<div className="flex justify-between items-center p-1">
+						<Icons.player_prev_button
+							onClick={() => player.prev()}
+							className="fill-black hover:fill-gray-600"
+						/>
+						<PlayOrPauseIcon
+							onClick={handlePlay}
+							className="ml-2 fill-black hover:fill-gray-600"
+						/>
+						<Icons.player_next_button
+							onClick={() => player.next()}
+							className="ml-2 fill-black hover:fill-gray-600"
+						/>
+					</div>
+					<div className="flex flex-col w-full h-full p-0 justify-start mx-2">
+						<div className="flex h-1/2 justify-between">
+							<div className="flex justify-center text-sm md:max-w-[500px] h-full mb-2">
+								<div className="leading-4">
+									{currentTrack ? currentTrack.title : "点击播放键开始播放"}
 								</div>
-								<div className="text-sm leading-4 ml-auto font-thin">{`${formatMMSS(
-									currentTime,
-								)}/${formatMMSS(duration)}`}</div>
+								<div className="leading-4 ml-2">
+									{currentTrack ? currentTrack.artist : ""}
+								</div>
 							</div>
-							<Slider
-								value={[currentTime]}
-								max={duration}
-								step={1}
-								onValueChange={(values) => {
-									const value = values[0];
-									player.seek(value);
-								}}
-							/>
+							<div className="text-sm leading-4 ml-auto font-thin">{`${formatMMSS(
+								currentTime,
+							)}/${formatMMSS(duration)}`}</div>
 						</div>
-						<div className="flex justify-center items-center p-4">
-							<PlayModeIcon
-								onClick={() => player.switchMode()}
-								className="fill-black hover:fill-gray-600"
-							/>
-							<Icons.player_close_button
-								className="ml-2 fill-black hover:fill-gray-600"
-								onClick={closePlayer}
-							/>
-						</div>
+						<Slider
+							value={[currentTime]}
+							max={duration}
+							step={1}
+							onValueChange={(values) => {
+								const value = values[0];
+								player.seek(value);
+							}}
+						/>
+					</div>
+					<div className="flex justify-center items-center p-4">
+						<PlayModeIcon
+							onClick={() => player.switchMode()}
+							className="fill-black hover:fill-gray-600"
+						/>
+						<Icons.player_close_button
+							className="ml-2 fill-black hover:fill-gray-600"
+							onClick={closePlayer}
+						/>
 					</div>
 				</div>
-			)}
-		</div>
+			</div>
+		</motion.div>
 	);
 }
