b82fe6515c069e35b55f6c67038dcf669f19b4ed
Yangchen Ye <eikasia30@gmail.com>
2025-07-12 19:55:42 -0400

    how about this

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
+    webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
   },
+
   images: {
     remotePatterns: [
       // Allow localhost for development
