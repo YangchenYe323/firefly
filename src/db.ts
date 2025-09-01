3542e37e6d432ab631d99457178cf0e47143f0d3
Yangchen Ye <eikasia30@gmail.com>
2025-09-01 18:24:09 -0400

    fix: sort out package version

--- src/db.ts
+++ src/db.ts
@@ -1,14 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from '@prisma/adapter-neon';
-import { neonConfig, Pool} from '@neondatabase/serverless';
+import { neonConfig } from '@neondatabase/serverless';
 import ws from 'ws';
 
 neonConfig.webSocketConstructor = ws;
 
 const prismaClientSingleton = () => {
 	const connectionString = process.env.POSTGRES_PRISMA_URL!;
-	const pool = new Pool({ connectionString });
-	const adapter = new PrismaNeon(pool);
+	const adapter = new PrismaNeon({ connectionString });
 	return new PrismaClient({ adapter });
 };
 
